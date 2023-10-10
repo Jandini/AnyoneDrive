a16c48fe1acf678569d271b0dcba88518452ed62
Matt Janda <matt.janda.kingston@gmail.com>
2023-10-11 00:19:04 +0100

    Update unit tests

--- src/AnyoneDrive.Tests/OneDriveFile_Must.cs
+++ src/AnyoneDrive.Tests/OneDriveFile_Must.cs
@@ -3,21 +3,22 @@ namespace AnyoneDrive.Tests
     public class OneDriveFile_Must
     {
 
+        const string TEST_SHARE = "https://1drv.ms/f/s!AuveTnis1UC4ylzSgCoulSxnQGB9";
 
-      
+
         [Fact]
         public async void Get_Stream()
         {
             var httpClient = new HttpClient();
-            var root = new OneDriveFolderInfo("https://1drv.ms/f/s!AuveTnis1UC4ylwrkuYqhdobhUGy?e=aq9M7h");
-            var folders = await root.GetFoldersAsync(httpClient, CancellationToken.None);
+            var root = new OneDriveFolderInfo(TEST_SHARE);
+            var folders = await root.GetFoldersAsync(httpClient);
 
             Assert.NotNull(folders);
             
-            var files = await folders.First().GetFilesAsync(httpClient, CancellationToken.None);
+            var files = await folders.First().GetFilesAsync(httpClient);
             Assert.Equal(5, files.Length);
 
-            using var stream = await files[0].GetStreamAsync(httpClient, CancellationToken.None);           
+            using var stream = await files[0].GetStreamAsync(httpClient);
         }
 
 
@@ -25,14 +26,14 @@ namespace AnyoneDrive.Tests
         public async void Read_Stream()
         {
             var httpClient = new HttpClient();
-            var root = new OneDriveFolderInfo("https://1drv.ms/f/s!AuveTnis1UC4ylwrkuYqhdobhUGy?e=aq9M7h");
-            var folders = await root.GetFoldersAsync(httpClient, CancellationToken.None);
+            var root = new OneDriveFolderInfo(TEST_SHARE);
+            var folders = await root.GetFoldersAsync(httpClient);
 
             Assert.NotNull(folders);
 
-            var files = await folders.First().GetFilesAsync(httpClient, CancellationToken.None);
+            var files = await folders.First().GetFilesAsync(httpClient);
                       
-            using var stream = await files[0].GetStreamAsync(httpClient, CancellationToken.None);
+            using var stream = await files[0].GetStreamAsync(httpClient);
 
             byte[] buffer = await stream.ReadBlockAsync(65536);
             Assert.Equal(5959, buffer.Length);
